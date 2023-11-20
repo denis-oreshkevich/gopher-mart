c6857aba613df974f28b134b79e59e61f3e69054
Areshkevich_D <DAreshkevich@iba.by>
2023-11-20 23:32:35 +0300

    remove regex:(

--- internal/app/api/user/user.go
+++ internal/app/api/user/user.go
@@ -10,19 +10,7 @@ import (
 	"go.uber.org/zap"
 	"io"
 	"net/http"
-	"regexp"
-)
-
-const (
-	emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
-		"^(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\" +
-		"\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)" +
-		"+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
-		"(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:" +
-		"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\" +
-		"\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$"
-
-	passwordRegex = "^[A-Za-z\\d@$!%*?&]{8,16}$"
+	"strings"
 )
 
 type Controller struct {
@@ -34,10 +22,6 @@ func NewController(svc *usvc.Service) *Controller {
 		svc: svc,
 	}
 }
-
-var emailMatcher = regexp.MustCompile(emailRegex)
-
-var passwordMatcher = regexp.MustCompile(passwordRegex)
 
 //go:generate easyjson user.go
 //easyjson:json
@@ -140,8 +124,10 @@ func readAndValidateUser(w http.ResponseWriter, r *http.Request) (AuthUser, bool
 var ErrUserValidation = errors.New("user validation error")
 
 func validateUser(user AuthUser) (ValidationResp, error) {
-	isEV := emailMatcher.MatchString(user.Login)
-	isPV := passwordMatcher.MatchString(user.Password)
+	login := strings.Trim(user.Login, " ")
+	isEV := len(login) > 0
+	pswd := strings.Trim(user.Password, " ")
+	isPV := len(pswd) > 0
 	var valErrors ValidationResp = make([]ValidationErrEntry, 0)
 	if !isEV {
 		validationErr := NewValidationErr("login", []string{"login is not valid"})
